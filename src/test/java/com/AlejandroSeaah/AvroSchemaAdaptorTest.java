752096617e9d740271d9cd9440f4c599c0e69c19
bjsheguihua <bjsheguihua@corp.netease.com>
2016-12-17 22:39:08 +0800

    update

--- src/test/java/com/AlejandroSeaah/AvroSchemaAdaptorTest.java
+++ src/test/java/com/AlejandroSeaah/AvroSchemaAdaptorTest.java
@@ -17,8 +17,8 @@ public class AvroSchemaAdaptorTest {
     @Test
     public void Test() throws DataProcessException{
 
-        String json = "{\"page\":\"FirstJson\",\"indexPath\":\"[-1,1]\",\"eventTime\":\"1480168647142\",\"view\":{\"path\":\"DecorView\\/LinearLayout[0]\\/FrameLayout[0]\\/RelativeLayout[0]\\/TabHost[0]\\/RelativeLayout[0]\\/FrameLayout[0]\\/DecorView[1]\\/LinearLayout[0]\\/FrameLayout[0]\\/LinearLayout[0]\\/CPRefreshableView[0]\\/ListView[0]\\/LinearLayout[-1,1]\",\"title\":\"\",\"frame\":\"{0,220,480,128}\",\"viewId\":\"df2f39d\",\"viewClass\":\"LinearLayout\"},\"eventName\":\"ListItemClick\"}";
-        String json2= "{\"page\":\"SecondJson\",\"indexPath\":\"[-1,2]\",\"eventTime\":\"1480168635950\",\"view\":{\"path\":\"DecorView\\/LinearLayout[0]\\/FrameLayout[0]\\/RelativeLayout[0]\\/LinearLayout[0]\\/FrameLayout[0]\\/NoSaveStateFrameLayout[0]\\/LinearLayout[0]\\/CPRefreshableView[0]\\/ClassifiedListView[0]\\/ListViewEx[0]\\/LinearLayout[-1,2]\",\"title\":\"\",\"frame\":\"{0,808,1080,236}\",\"viewId\":\"7194afeb\",\"viewClass\":\"LinearLayout\"},\"eventName\":\"ListItemClick\"}";
+        String json = "{\"page\":\"FirstJson\",\"indexPath\":\"[-1,1]\",\"eventTime\":\"1480168647142\",\"view\":{\"path\":\"DecorView\\/LinearLayout[0]\\/FrameLayout[0]\\/RelativeLayout[0]\\/TabHost[0]\\/RelativeLayout[0]\\/FrameLayout[0]\\/DecorView[1]\\/LinearLayout[0]\\/FrameLayout[0]\\/LinearLayout[0]\\/CPRefreshableView[0]\\/ListView[0]\\/LinearLayout[-1,1]\",\"title\":\"\",\"frame\":\"{0,220,480,128}\",\"viewId\":\"df2f39d\",\"viewClass\":\"LinearLayout\"}}";
+        String json2= "{\"page\":\"SecondJson\",\"indexPath\":\"[-1,2]\",\"eventTime\":\"1480168635950\",\"view\":{\"path\":\"DecorView\\/LinearLayout[0]\\/FrameLayout[0]\\/RelativeLayout[0]\\/LinearLayout[0]\\/FrameLayout[0]\\/NoSaveStateFrameLayout[0]\\/LinearLayout[0]\\/CPRefreshableView[0]\\/ClassifiedListView[0]\\/ListViewEx[0]\\/LinearLayout[-1,2]\",\"title\":\"\",\"frame\":\"{0,808,1080,236}\",\"viewId\":\"7194afeb\",\"viewClass\":\"LinearLayout\"}}";
         String autoAvroSchema = null;
         try{
             autoAvroSchema = AvroSchemaAdaptor.avroSchemaAutoAdaption(json);
